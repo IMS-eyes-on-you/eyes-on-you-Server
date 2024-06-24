87c3dfddb62b94c64190927b752038b2735edb2b
bnh5992 <skagus159632@naver.com>
2024-06-24 15:43:34 +0900

    test

--- src/main/java/com/example/steam/controller/MessageController.java
+++ src/main/java/com/example/steam/controller/MessageController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
     @GetMapping("/a")
     public String test(){
-        return "hello";
+        return "hello2";
     }
 }
