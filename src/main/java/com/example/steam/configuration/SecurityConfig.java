2e1fe665bbf595fe9204139ed520cb98a2894c43
bnh4924 <skagus159632@naver.com>
2024-08-12 13:09:24 +0900

    securityConfig

--- src/main/java/com/example/steam/configuration/SecurityConfig.java
+++ src/main/java/com/example/steam/configuration/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.example.steam.configuration;
 
+import com.example.steam.auth.Service.CustomOAuth2UserService;
+import com.example.steam.auth.filter.JwtAuthFilter;
+import com.example.steam.auth.handler.MyAuthenticationSuccessHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,22 +12,31 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final MyAuthenticationSuccessHandler loginSuccessHandler;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable) // 1번
+                .csrf(AbstractHttpConfigurer::disable)
                 .headers((headerConfig) ->
                         headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
                 )
                 .authorizeHttpRequests((authorizeRequests) ->
                         authorizeRequests
-                                .requestMatchers("/**").permitAll()
+                                .requestMatchers("/signal").permitAll()
+                                .requestMatchers("/auth/signup").permitAll()
+                                .requestMatchers("/auth/signin").permitAll()
                                 .anyRequest().authenticated()
-                );
+                )
+                //oauth2Login(oauth ->oauth.userInfoEndpoint(c -> c.userService(loginSuccessHandler)).successHandler())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
