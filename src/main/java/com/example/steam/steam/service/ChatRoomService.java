772db946f6ac9b42698b5104a8b72377673f0f3b
bnh4924 <skagus159632@naver.com>
2024-08-13 15:54:03 +0900

    [fix] error

--- src/main/java/com/example/steam/steam/service/ChatRoomService.java
+++ src/main/java/com/example/steam/steam/service/ChatRoomService.java
@@ -1,16 +1,20 @@
 package com.example.steam.steam.service;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Setter
 @Getter
-@Component
+@RequiredArgsConstructor
+@Service
 public class ChatRoomService {
     private SseEmitter emitter;
-
+    
     public void notifyExitClass() {
         if (emitter != null) {
             try {
