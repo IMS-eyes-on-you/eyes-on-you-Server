ecf7ad9c80b5d3623c327cf2ab2b187ea1b248a5
bnh4924 <skagus159632@naver.com>
2024-08-13 15:39:38 +0900

    bean 주입

--- src/main/java/com/example/steam/steam/service/ChatRoomService.java
+++ src/main/java/com/example/steam/steam/service/ChatRoomService.java
@@ -1,17 +1,15 @@
 package com.example.steam.steam.service;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+@Setter
+@Getter
+@Component
 public class ChatRoomService {
     private SseEmitter emitter;
-
-    public SseEmitter getEmitter() {
-        return emitter;
-    }
-
-    public void setEmitter(SseEmitter emitter) {
-        this.emitter = emitter;
-    }
 
     public void notifyExitClass() {
         if (emitter != null) {
